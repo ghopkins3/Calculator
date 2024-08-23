0f1f3c9dbeffda57eaac7f11f3f9f6a2b9f90ec2
ghopkins3 <ghopkins3@murraystate.edu>
2024-08-23 18:46:41 -0500

    Added all buttons/button designs, added symbols at top and clock, added i/o display

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+
 let x = 0;
 let y = 0;
 let op = 0;
@@ -29,3 +30,26 @@ function operate(x, y, op) {
         return multiply(x, y);
     }
 }
+
+function showTime() {
+    const dateDisplay = document.querySelector("#time");
+
+    let time = new Date();
+    let hour = time.getHours();
+    let minute = time.getMinutes();
+    let meridiem = "am";
+
+    if(hour >= 12) {
+        if(hour > 12) hour -= 12;
+        meridiem = "pm";
+    } else if(hour == 0) {
+        hour = 12;
+        meridiem = "am";
+    }
+
+    let currentTime = hour + ":" + minute + " " + meridiem;
+    dateDisplay.textContent = currentTime;
+}
+
+showTime();
+setInterval(showTime, 1000);
